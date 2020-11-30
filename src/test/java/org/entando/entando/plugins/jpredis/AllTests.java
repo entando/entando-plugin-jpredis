2359edf5e1dbc0da91c21e55dae50fdd2b897ee4
Ampie Barnard <ampie.private@gmail.com>
2020-11-30 16:10:31 +0200

    Trying to generate test results again

---
+++ src/test/java/org/entando/entando/plugins/jpredis/AllTests.java
@@ -0,0 +1,12 @@
+package org.entando.entando.plugins.jpredis;
+
+import org.entando.entando.plugins.jpredis.aps.system.redis.CacheConfig;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({CacheConfigTest.class})
+public class AllTests {
+
+}
