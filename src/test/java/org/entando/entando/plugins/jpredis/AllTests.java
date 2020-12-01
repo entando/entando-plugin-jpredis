3c853ca74fd014802c984cacdd1e429427ccade4
Eugenio Santoboni <eugenio.santoboni@gmail.com>
2020-12-01 15:53:57 +0100

    ENG-1564: Added first integration test

--- src/test/java/org/entando/entando/plugins/jpredis/AllTests.java
+++ src/test/java/org/entando/entando/plugins/jpredis/AllTests.java
@@ -1,12 +1,24 @@
 package org.entando.entando.plugins.jpredis;
 
+import junit.framework.Test;
+import junit.framework.TestSuite;
 import org.entando.entando.plugins.jpredis.aps.system.redis.CacheConfig;
+import org.entando.entando.plugins.jpredis.aps.system.services.CacheInfoManagerIntegrationTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
-@RunWith(Suite.class)
-@SuiteClasses({CacheConfigTest.class})
 public class AllTests {
+
+    public static Test suite() {
+        TestSuite suite = new TestSuite("Test for Redis connector");
+
+        System.out.println("Test for Redis connector");
+        
+        //
+        suite.addTestSuite(CacheInfoManagerIntegrationTest.class);
+
+        return suite;
+    }
 
 }
